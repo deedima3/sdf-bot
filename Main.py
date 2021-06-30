344ff8023f86c8b88bbee425666b28f74e5dbfe8
deedima3 <54178249+deedima3@users.noreply.github.com>
2021-06-30 21:06:45 +0800

    update start

--- Main.py
+++ Main.py
@@ -49,7 +49,13 @@ def handle_message(event):
             event.reply.token, TextSendMessage(text="PlaceHolder menu SDF")
         )
 
+
 if __name__ == "__main__":
-    #    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
