ad949d1706c195525e2620d8683e6b29de4c2395
deedima3 <54178249+deedima3@users.noreply.github.com>
2021-07-01 13:00:31 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -171,7 +171,7 @@ def handle_message(event):
             "https://simdos.unud.ac.id/uploads/file_foto_dir/c846fc105e2f2f845a4015fc8edeaa90fd0088db.jpg",
             GANDHIADI_TEXT,
             "https://udayananetworking.unud.ac.id/lecturer/2268-gk-gandhiadi"
-            "l",
+            "lg",
             "3:4"
         )
         bubble3 = createBubble(
@@ -179,7 +179,7 @@ def handle_message(event):
             "https://simdos.unud.ac.id/uploads/file_foto_dir/7b479289d3cc1ab53944113d03763d70.jpg",
             SUKADANA_TEXT,
             "https://udayananetworking.unud.ac.id/lecturer/2326-i-made-sukadana"
-            "l",
+            "lg",
             "3:4"
         )
         bubble4 = createBubble(
@@ -187,7 +187,7 @@ def handle_message(event):
             "https://udayananetworking.unud.ac.id/protected/storage/foto_biografi/foto%203x4.jpg",
             GUNAWAN_TEXT,
             "https://udayananetworking.unud.ac.id/professor/credential/2298-i-wayan-gede-gunawan"
-            "l",
+            "lg",
             "3:4"
         )
         container = CarouselContainer(contents=[bubble1, bubble2, bubble3, bubble4])
